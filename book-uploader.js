6ed88217ea97b12668b6c169f44c480c9dabbc36
astrosshow <130030251+astrosshow@users.noreply.github.com>
2025-01-11 12:00:41 -0800

    Fixed first page of the pdf rendering

--- book-uploader.js
+++ book-uploader.js
@@ -1,28 +1,42 @@
+import {extractFirstImage} from "./pdf-title-image.js";
+
+// Get references to key DOM elements
 const uploadButton = document.getElementById('upload-button')
 const uploadInput = document.getElementById('upload-pdf')
 const booksContainer = document.getElementById('books-container')
 
-uploadButton.addEventListener('click', () => {
+uploadButton.addEventListener('click', async () => {
     const files = uploadInput.files;
 
     if (!files.length) {
         alert('Please select a file to upload.')
     }
 
-    Array.from(files).forEach((file) => {
+    for (const file of files) {
         if (file.type !== 'application/pdf') {
-            alert(`${file.name} is not a valid PDF.`)
+            alert('Not a valid PDF file.');
         }
 
-        const bookTitle = file.name.replace('.pdf', '')
-        const bookElement = createBookElement(bookTitle, file);
-        booksContainer.appendChild(bookElement)
-    })
+        const bookTitle = file.name.replace('.pdf', '');
+
+        // Extract first image from pdf
+        const pdfURL = URL.createObjectURL(file);
+        let bookImageURL = 'https://via.placeholder.com/150';
+
+        try {
+            bookImageURL = (await extractFirstImage(pdfURL)) || bookImageURL;
+        } catch (error) {
+            console.error(`Error extracting image from ${bookTitle}:`, error);
+        }
+
+        const bookElement = createBookElement(bookTitle, file, bookImageURL);
+        booksContainer.appendChild(bookElement);
+    }
 
     uploadInput.value = '';
 })
 
-function createBookElement(title, file) {
+function createBookElement(title, file, bookImageURL) {
     const bookDiv = document.createElement('div');
     bookDiv.className = 'book';
 
@@ -31,7 +45,7 @@ function createBookElement(title, file) {
 
     const bookImage = document.createElement('img');
     // Hardcoded: Replace URL
-    bookImage.src = 'https://via.placeholder.com/150';
+    bookImage.src = bookImageURL;
     bookImage.alt = title;
 
     // PDF viewer
